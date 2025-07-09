2bf7d77dd418a33f2b73862752a0ae2ab3570943
unknown <subhashofficial411@gmail.com>
2025-07-10 01:26:47 +0530

    Updated  serializers,permissions,url,and views in course app

--- course/urls.py
+++ course/urls.py
@@ -1,3 +1,18 @@
 from django.urls import path
+from .views import (
+    CategoryListView,
+    CourseListView,
+    CourseDetailView,
+    ReviewListCreateView,
+    ReviewDetailView,
+    FAQListCreateView
+)
 
-urlpatterns=[]
+urlpatterns = [
+    path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('courses/', CourseListView.as_view(), name='course-list'),
+    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
+    path('reviews/', ReviewListCreateView.as_view(), name='review-list-create'),
+    path('reviews/<int:pk>/', ReviewDetailView.as_view(), name='review-detail'),
+    path('faqs/', FAQListCreateView.as_view(), name='faq-list-create'),
+]
