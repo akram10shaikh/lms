695913a2e78c25be657c192bfd7988fd774ae04d
kummitisowmya <sowmyakummiti@gmail.com>
2025-07-10 12:35:09 +0530

    adding category functionality

--- course/serializers.py
+++ course/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
+from .models import Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+    def validate_name(self, value):
+        if Category.objects.filter(name=value).exists():
+            raise serializers.ValidationError("Category name must be unique.")
+        return value
 
--- course/urls.py
+++ course/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import CategoryListCreateAPIView, CategoryDetailAPIView
+
+urlpatterns = [
+    path('categories/',CategoryListCreateAPIView.as_view(), name='category-list-create'),
+    path('categories/<int:pk>/',CategoryDetailAPIView.as_view(), name='category-detail'),
+]
 
