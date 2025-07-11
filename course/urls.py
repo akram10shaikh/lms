41412113cb75bfb79b368e52f6830d4910789074
Ashok-20ARC <ashok20arc@gmail.com>
2025-07-11 15:06:24 +0530

    Adding top new courses filter and course detail

--- course/urls.py
+++ course/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import CategoryListCreateAPIView, CategoryDetailAPIView
+from .views import CategoryListCreateAPIView, CategoryDetailAPIView,CourseDetailAPIView,TopNewCourseAPIView,TopNewCourseDetailAPIView
 
 urlpatterns = [
     path('categories/',CategoryListCreateAPIView.as_view(), name='category-list-create'),
     path('categories/<int:pk>/',CategoryDetailAPIView.as_view(), name='category-detail'),
+    path('course/<int:pk>/',CourseDetailAPIView.as_view(),name='course-detail'),
+    path('course/',CourseDetailAPIView.as_view(),name='course-detail'),
+    path('top-new/',TopNewCourseAPIView.as_view(),name='top-new-courses'),
+    path('top-new/<int:pk>/',TopNewCourseDetailAPIView.as_view(),name='top-new-course-detail'),
 ]
 
