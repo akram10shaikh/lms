4203df97307a6a2aa3338cef32d0851dbfb5ab79
Alok-ganguly <ganguly.alok1493@gmail.com>
2025-07-12 01:12:13 +0530

    Added Trending Courses, Trending course detail and all courses API

--- course/urls.py
+++ course/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import CategoryListCreateAPIView, CategoryDetailAPIView,CourseDetailAPIView,TopNewCourseAPIView,TopNewCourseDetailAPIView
+from .views import CategoryListCreateAPIView, CategoryDetailAPIView, CourseDetailAPIView, TopNewCourseAPIView, \
+    TopNewCourseDetailAPIView, TrendingCourseAPIView, TrendingCourseDetailAPIView, AllCoursesAPIView
 
 from .views import ReviewListCreateView, ReviewDetailView
 
@@ -10,6 +11,9 @@ urlpatterns = [
     path('categories/<int:pk>/',CategoryDetailAPIView.as_view(), name='category-detail'),
     path('course/<int:pk>/',CourseDetailAPIView.as_view(),name='course-detail'),
     path('course/',CourseDetailAPIView.as_view(),name='course-detail'),
+    path('trending/', TrendingCourseAPIView.as_view(), name='trending-courses'),
+    path('trending/<int:pk>/', TrendingCourseDetailAPIView.as_view(), name='trending-course-detail'),
+    path('all/', AllCoursesAPIView.as_view(), name='all-courses'),
     path('top-new/',TopNewCourseAPIView.as_view(),name='top-new-courses'),
     path('top-new/<int:pk>/',TopNewCourseDetailAPIView.as_view(),name='top-new-course-detail'),
     path('reviews/', ReviewListCreateView.as_view(), name='review-list-create'),
