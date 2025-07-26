ed66f326b28ab88d41a41ce0c4ed2b88429e7604
unknown <subhashofficial411@gmail.com>
2025-07-26 07:36:21 +0530

    Updated Batch-App

--- batch/views.py
+++ batch/views.py
@@ -4,13 +4,11 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Batch, BatchStudent
 from .serializers import BatchSerializer, BatchStudentSerializer
 
-
 class BatchListCreateView(generics.ListCreateAPIView):
     queryset = Batch.objects.all()
     serializer_class = BatchSerializer
-    authentication_classes = [JWTAuthentication, SessionAuthentication]  # ✅ support both
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
     permission_classes = [permissions.IsAuthenticated]
-
 
 class BatchDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Batch.objects.all()
@@ -18,13 +16,11 @@ class BatchDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication, SessionAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
-
 class BatchStudentListCreateView(generics.ListCreateAPIView):
     queryset = BatchStudent.objects.all()
     serializer_class = BatchStudentSerializer
     authentication_classes = [JWTAuthentication, SessionAuthentication]
     permission_classes = [permissions.IsAuthenticated]
-
 
 class BatchStudentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BatchStudent.objects.all()
